7ae1cddc36bba9ac27033a924acb11d7db3ef6de
Nadina <sheriefnadina@gmail.com>
2024-06-13 23:30:52 +0200

    commit

--- client/src/components/featProperties/FeatProperties.jsx
+++ client/src/components/featProperties/FeatProperties.jsx
@@ -1,4 +1,4 @@
-import useFetch from "../../hooks/useFetch.js";
+/* import useFetch from "../../hooks/useFetch.js";
 import "./featProperties.css";
 
 const FeaturedProperties = () => {
@@ -28,6 +28,74 @@ const FeaturedProperties = () => {
           ))}
         </>
       )}
+    </div>
+  );
+};
+
+export default FeaturedProperties; */
+
+
+import "./featProperties.css";
+
+const FeaturedProperties = () => {
+  return (
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/215955381.jpg?k=ff739d1d9e0c8e233f78ee3ced82743ef0355e925df8db7135d83b55a00ca07a&o=&hp=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Comfort Suites Airport</span>
+        <span className="fpCity">Austin</span>
+        <span className="fpPrice">Starting from $140</span>
+        <div className="fpRating">
+          <button>9.3</button>
+          <span>Exceptional</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.travalliancemedia.com%2Fimages%2F99999999-9999-9999-9999-999999999999%2F29f0e80d-a7bc-e611-9aa9-0050568e420d%2Fsource.jpg&f=1&nofb=1&ipt=787a49efde5b2bae7e0fca01d187c7ddff19a9e4d8017a341ed8396e70efc34a&ipo=images"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Four Seasons Hotel</span>
+        <span className="fpCity">Lisbon</span>
+        <span className="fpPrice">Starting from $99</span>
+        <div className="fpRating">
+          <button>8.8</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.raymondteam.com%2Fwp-content%2Fuploads%2F2020%2F06%2FORDCH-Exterior-Dusk.jpg&f=1&nofb=1&ipt=a43538f941ad0c0c9436805df74cb64608a4afcb2835587b857aef198221c8e6&ipo=images"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Hilton Garden Inn</span>
+        <span className="fpCity">Berlin</span>
+        <span className="fpPrice">Starting from $105</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
     </div>
   );
 };
