f4b2bed78b16d2c15c8a02a99875469c008ccfea
Nadina <sheriefnadina@gmail.com>
2024-06-13 22:01:30 +0200

    first commit

---
+++ api/index.js
@@ -0,0 +1,62 @@
+import express from "express";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+
+const app = express();
+dotenv.config();
+
+const { EXPRESS_PORT, MONGO_URI } = process.env;
+
+const connect = async () => {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log("Connected to mongoDB.");
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!");
+});
+
+//middlewares
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["https://bookig-app.vercel.app"],
+    credentials: true,
+  })
+); 
+
+
+app.use("/auth", authRoute);
+app.use("/users", usersRoute);
+app.use("/hotels", hotelsRoute);
+app.use("/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+      success: false,
+      status: errorStatus,
+      message: errorMessage,
+      stack: err.stack,
+    });
+  });
+  
+  app.listen(EXPRESS_PORT, () => {
+    connect();
+    console.log("Connected to backend.");
+  });
