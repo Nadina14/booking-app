8009e531d0d916fe5bb3a6ee523419df281c274a
Nadina <sheriefnadina@gmail.com>
2024-06-14 19:06:19 +0200

    commit1

--- api/models/User.js
+++ api/models/User.js
@@ -1,4 +1,9 @@
 import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+import validator from "validator";
+import { comparePassword, hiddenPassword } from "../library/cryptingPass.js";
+const { isStrongPassword, isEmail } = validator;
+
 
 const UserSchema = new mongoose.Schema(
   {
@@ -38,5 +43,61 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+UserSchema.statics.findByEmail = function (email) {
+  return this.findOne({ email });
+};
+
+UserSchema.statics.signUp = async function (username, email, password) {
+  if (!isEmail(email)) {
+    const error = new Error("You not send a real email.");
+    error.statusCode = 400;
+    throw error;
+  }
+  if (!isStrongPassword(password, strongPasswordOptions)) {
+    const error = new Error("Password not strong.");
+    error.statusCode = 400;
+    throw error;
+  }
+
+  const emailEx = await this.exists({ email });
+  if (emailEx) {
+    const error = new Error("Account already exists.");
+    error.statusCode = 400;
+    throw error;
+  }
+  const hashedPassword = await hiddenPassword(password);
+
+  const user = await this.create({ username, email, password: hashedPassword });
+  return user;
+};
+
+UserSchema.statics.logIn = async function (email, password) {
+  const user = await this.findByEmail(email);
+  const failError = () => {
+    const error = new Error("Email or Password not valid.");
+    error.statusCode = 401;
+    throw error;
+  };
+
+  if (!user) {
+    failError();
+  }
+
+  const passwordMatch = await comparePassword(password, user.password);
+  if (!passwordMatch) {
+    failError();
+  }
+
+  return user;
+};
+
+UserSchema.methods.clean = function () {
+  const user = this.toObject();
+  delete user.password;
+  delete user.__v;
+  delete user._id;
+  return user;
+};
 
 export default mongoose.model("User", UserSchema);
--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home.jsx';
-import List from './pages/List/List.jsx';
-import Hotel from './pages/Hotel/Hotel.jsx';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Hotel from "./pages/Hotel/Hotel.jsx";
+import List from "./pages/List/List.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Registration from "./pages/Register/Register.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/hotels' element={<List/>}/>
-          <Route path='/hotels/:id' element={<Hotel/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
+        <Route path="/registration" element={<Registration/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
