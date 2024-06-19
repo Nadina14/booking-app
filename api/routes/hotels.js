e1211bbc754a3dde1fb787cba79bece1c16c0894
Nadina <sheriefnadina@gmail.com>
2024-06-19 07:44:22 +0200

    final change api

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,6 +1,4 @@
 import express from "express";
-import Hotel from "../models/Hotel.js";
-import { createError } from "../utils/error.js";
 import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotelRooms, getHotels, updateHotel } from "../controllers/hotelController.js";
 import {verifyAdmin} from "../utils/verifyToken.js"
 
